141d4901a7cbf8a9d0ab2c43dfc9944b003453f1
Sam Hartharn-Evans <57717460+HartharnSam@users.noreply.github.com>
2022-11-17 09:16:25 +0000

    Version 2 Large Float Tracking

--- DigiFlow_read/particletracking/track_largefloats.m
+++ DigiFlow_read/particletracking/track_largefloats.m
@@ -1,25 +1,67 @@
-% Plot to try and figure out float tracking for large floats
-% use on 180322_A
-%
 clc; clearvars; close all; 
 
-ice_threshold = 180/255;
-
+ice_threshold = 160/255;
+figure(1);
 im = dfireadvel('ice_ts.dfi');
 grid = dfi_grid_read(im);
+data = im.cdata(:, :, 1);
+data(data<ice_threshold) = NaN;
 
-imagesc(grid.x, grid.y, im.cdata(:, :, 1));
+imagesc(grid.x, grid.y, data);
 colormap('gray')
-[brightnesses, x_inds] = sort(im.cdata(:, :, 1), 2);
 
-xs = grid.xi(x_inds);
+%% Detect peaks
+figure(3); clf
+figure(2); clf;
 
-%inds = (brightnesses(:, end)>ice_threshold) && abs(brightnesses(:, end)-brightnesses(:, ));
+xpeaks = nan(1, grid.ny); widths = xpeaks;
+for ii = 1:grid.ny
+
+    %figure(2);
+    %plot(grid.xi, data(ii, :));
+    
+    % Identify the peak initially
+    [~, xpeaks_tmp, widths_tmp] = findpeaks(flip(data(ii, :)), flip(grid.xi),'SortStr','descend',...
+        'NPeaks', 5);
+    % TODO: Add in flat peak detection, and adjust the location of the flat
+    % to the centre instead?
+
+    % Make a prediction based on the previous timestep
+    if ii > 1 && ~isnan(xpeaks(ii-1)) length(xpeaks_tmp)>=1;
+        region = -25*widths(ii-1).^2 + widths(ii-1)
+        xpeak_min = xpeaks(ii-1)-region/2;
+        xpeak_max = xpeaks(ii-1)+region/2;
+        
+        if xpeaks_tmp(1)<xpeak_min || xpeaks_tmp(1)>xpeak_max
+            xpeakind = find(xpeaks_tmp>xpeak_min & xpeaks_tmp<xpeak_max);
+            if length(xpeakind)>=1
+                xpeaks_tmp(1) = xpeaks_tmp(xpeakind(1));
+                widths_tmp(1) = widths_tmp(1);
+            else
+                warning(['On ii = ', num2str(ii), ' Prediction failed!'])
+                xpeaks_tmp(1) = NaN;
+                widths_tmp(1) = NaN;
+            end
+
+        end
+    end
+    if ~isempty(xpeaks_tmp)
+        xpeaks(ii) = xpeaks_tmp(1);
+        widths(ii) = widths_tmp(1);
+        hold on
+        plot(xpeaks(ii), .9, 'kx')
+        hold off
+    else
+        xpeaks(ii) = NaN; widths(ii) = NaN;
+    end
+
+    %xpeaks{ii} = grid.xi(peaks{ii});
+
+
+    %pause(.05);
+
+end
+
+figure(1);
 hold on
-ranges = range(xs(:, end-5:end), 2);
-plot(ranges+4.7, grid.yi, 'bx')
-inds = ranges < 0.01;
-plot(ranges(~inds)+4.7, grid.yi(~inds), 'rx')
-
-hold on
-plot(xs(inds, end), grid.yi(inds));
+plot(xpeaks, grid.yi, 'rx')
